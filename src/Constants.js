c5eb80f96b63416f65d4443213edf15eb6c94dfb
Toàn Nguyễn <65778790+Toanbrasco@users.noreply.github.com>
2022-08-21 01:20:06 +0700

    Fix Constants

--- src/Constants.js
+++ src/Constants.js
@@ -138,9 +138,5 @@ export const makeNumArr = num => new Array(num).fill("").map((_, i) => i + 1);
 
 export const UrlApi = 'https://smartgarden-server-test.herokuapp.com'
 
-// 'http://localhost:5000'
-export const SESSION_STORAGE_TOKEN_NAME = 'smart-garden'
-
-export const UrlApi = 'https://smartgarden-server-test.herokuapp.com'
-//       'http://localhost:5000'
+//'http://localhost:5000'
 export const SESSION_STORAGE_TOKEN_NAME = 'smart-garden'
